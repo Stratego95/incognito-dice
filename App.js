e53381b70bcb4fe256f2ab595b438d9a34ad02bd
Jul Eig <juleig@web.de>
2021-04-25 00:14:40 +0200

    add sound, shake event, code cleanup, prettier

--- App.js
+++ App.js
@@ -1,48 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
-import Marble from './Marble';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
+import Marble from "./Marble";
+import { Audio } from "expo-av";
+import { ShakeEventExpo } from "./ShakeDevice";
 
 export default function App() {
-
+  const [sound, setSound] = useState();
   const [marble1, setMarble1] = useState("white");
   const [marble2, setMarble2] = useState("white");
   const [marble3, setMarble3] = useState("white");
 
+  useEffect(() => {
+    async function shake() {
+      ShakeEventExpo.addListener(() => {
+        calculateDiceColors();
+      });
+    }
+    shake();
+  }, []);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   const calculateDiceColors = () => {
-    let availableColors = ["blue", "blue", "red", "red", "yellow", "black", "black", "white", "white", "white"];
+    playSound();
+    let availableColors = [
+      "blue",
+      "blue",
+      "red",
+      "red",
+      "yellow",
+      "black",
+      "black",
+      "white",
+      "white",
+      "white",
+    ];
 
     let idx = Math.floor(Math.random() * availableColors.length);
     setMarble1(availableColors[idx]);
-    availableColors = [...availableColors.slice(0, idx), ...availableColors.slice(idx+1)];
+    availableColors = [
+      ...availableColors.slice(0, idx),
+      ...availableColors.slice(idx + 1),
+    ];
 
     idx = Math.floor(Math.random() * availableColors.length);
     setMarble2(availableColors[idx]);
-    availableColors = [...availableColors.slice(0, idx), ...availableColors.slice(idx+1)];
+    availableColors = [
+      ...availableColors.slice(0, idx),
+      ...availableColors.slice(idx + 1),
+    ];
 
     idx = Math.floor(Math.random() * availableColors.length);
     setMarble3(availableColors[idx]);
-    availableColors = [...availableColors.slice(0, idx), ...availableColors.slice(idx+1)];
+    availableColors = [
+      ...availableColors.slice(0, idx),
+      ...availableColors.slice(idx + 1),
+    ];
+  };
+
+  const playSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/dice.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync();
   };
 
   return (
     <TouchableWithoutFeedback onPress={() => calculateDiceColors()}>
-      <View style={styles.container}>
-          <Marble color={marble1}/>
-          <Marble color={marble2}/>
-          <Marble color={marble3}/>
-          <StatusBar style="auto" />
+      <View style={styles.container1}>
+        <View style={styles.container}>
+          <Marble color={marble1} />
+          <Marble color={marble2} />
+          <Marble color={marble3} />
+        </View>
       </View>
     </TouchableWithoutFeedback>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container1: {
     flex: 1,
-    backgroundColor: 'grey',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- Marble.js
+++ Marble.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-const Marble = ({color}) => {
-
-    return(
-        <View>
-            <View style={{width: 100, height: 100, backgroundColor: color, margin: 5, borderRadius:50}}></View>
-        </View>
-    )
+const Marble = ({ color }) => {
+  return (
+    <View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: color,
+          margin: 5,
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 };
 
 export default Marble;
