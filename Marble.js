b52278923df4a5f4154e47f76b19eee5029c61c3
Jul Eig <juleig@web.de>
2021-04-24 23:18:26 +0200

    cleanuo marble

--- Marble.js
+++ Marble.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {View} from 'react-native';
 
 const Marble = ({color}) => {
+
     return(
         <View>
-            <View style={{width: 50, height: 50, backgroundColor: color, margin: 5}}></View>
+            <View style={{width: 100, height: 100, backgroundColor: color, margin: 5, borderRadius:50}}></View>
         </View>
     )
 };
-
-// const styles = StyleSheet.create({
-//     container: {
-//         width: 50,
-//         height: 50,
-//         backgroundColor: {globalStupidColor},
-//         margin: 5,
-//     },
-//   });
 
 export default Marble;
